60390c0f3e791441c37c92b7f60e3b5102e1c8c4
Arunkumar P S <arunkumar96293@gmail.com>
2020-05-18 16:05:41 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,5 +1,9 @@
 #!/usr/bin/env groovy
 
-def call(String name = 'build') {
-  sh 'mvn clean install'
+//def call(String name = 'build') {
+ // sh 'mvn clean install'
+//}
+
+def call(String name = 'human') {
+  echo "Hello, ${name}."
 }
