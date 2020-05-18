76a888e891bbdf324ae5a1ea13e1e6f2d17a3a30
Arunkumar P S <arunkumar96293@gmail.com>
2020-05-18 17:22:07 +0530

    Update hello.groovy

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,9 +1,12 @@
 #!/usr/bin/env groovy
 
-//def call(String name = 'build') {
- // sh 'mvn clean install'
-//}
-
-def call(String name = 'human') {
-  echo "Hello, ${name}."
+def info(build) {
+  sh 'mvn clean install'
 }
+
+//def call(String name = 'human') {
+//  echo "Hello, ${name}."
+//}
+//def info(message) {
+//    echo "INFO: ${message}"
+//}
