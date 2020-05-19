087afab08aea768716b4f6febb6724bf225075a8
Arunkumar P S <arunkumar96293@gmail.com>
2020-05-19 16:19:38 +0530

    Update dockbuild.groovy

--- vars/dockbuild.groovy
+++ vars/dockbuild.groovy
@@ -1,4 +1,4 @@
-def call(String hubuser, String repo) {
+def call(String hubuser, String repo,String repotag) {
     withCredentials([usernamePassword(
             credentialsId: "Dockerhub",
             usernameVariable: "Username",
@@ -6,7 +6,7 @@ def call(String hubuser, String repo) {
     )]) {
         sh "docker login -u '$Username' -p '$Password'"
     }
-    sh "docker image build -t ${hubUser}/${repo}:petclinic-v${env.BUILD_NUMBER} ."
-    sh "docker image push ${hubUser}/${repo}:petclinic-v${env.BUILD_NUMBER}"
-    sh "docker image rm ${hubUser}/${repo}:petclinic-v${env.BUILD_NUMBER}"   
+    sh "docker image build -t ${hubUser}/${repo}:${repotag}-v${env.BUILD_NUMBER} ."
+    sh "docker image push ${hubUser}/${repo}:${repotag}-v${env.BUILD_NUMBER}"
+    sh "docker image rm ${hubUser}/${repo}:${repotag}-v${env.BUILD_NUMBER}"   
 }
