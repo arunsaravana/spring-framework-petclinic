20ad42c1fdfcad2f929eab978469f9f059746e0f
Arunkumar P S <arunkumar96293@gmail.com>
2020-05-21 11:56:30 +0530

    Update kubeupdate.groovy

--- vars/kubeupdate.groovy
+++ vars/kubeupdate.groovy
@@ -1,9 +1,9 @@
-def call(String awslogin, String region, String ekscluster) {
- withCredentials([[
- $class: 'AmazonWebServicesCredentialsBinding', 
- accessKeyVariable: 'AWS_ACCESS_KEY_ID', 
- credentialsId: '${awslogin}', 
- secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) 
+def call(String region, String ekscluster) {
+// withCredentials([[
+// $class: 'AmazonWebServicesCredentialsBinding', 
+// accessKeyVariable: 'AWS_ACCESS_KEY_ID', 
+// credentialsId: '${awslogin}', 
+// secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) 
  {
               echo "Login Successfull"
               sh "aws eks --region '${region}'  update-kubeconfig --name '${ekscluster}'"
