02ad9455b26af42d8c437f326e71a410e910bc16
Arunkumar P S <arunkumar96293@gmail.com>
2020-05-28 17:28:21 +0530

    Update sonaranalysis.groovy

--- vars/sonaranalysis.groovy
+++ vars/sonaranalysis.groovy
@@ -1,6 +1,3 @@
-def call(String Sonarserver, String scanner, String scannerproperties) {
-   //def scannerHome = tool '${scanner}';
-   withSonarQubeEnv('${Sonarserver}') {
-      sh "/opt/sonar-scanner/bin/sonar-scanner -Dproject.settings=${scannerproperties}"    
-   }
+def call() {
+  sh 'mvn sonar:sonar'
 }
